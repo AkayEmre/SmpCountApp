941143d23f58d414e433ed5ba2eddc29d694d79a
Emre Akay <emre.akayy@hotmail.com>
2024-07-18 21:25:23 +0300

    Second line added with strftime

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import time
 
+print(f"Güncel Saat: {time.strftime('%X')}")
+
+def simple_counter(seconds):
+    for i in range(seconds,0,-1):
+        print(i)
+        time.sleep(1)  # 1 saniye bekler
+
+# Sayaç süresi (saniye)
+counter_duration = int(input(f"Sayaç Süresini giriniz: "))
+
+
+# Sayaç başlatma
+simple_counter(counter_duration)
+
+print("Süre Doldu")
